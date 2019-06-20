a80f5a7e0ad9d733649c05db82297ea4dafaa90d
liwendiao <liwnediaodeyouxian@163.com>
2019-06-20 23:34:18 +0800

    20190620

---
+++ src/Libraries/Nop.Services/AIBookModel/AiBookService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Nop.Core;
+using Nop.Core.Domain.AIBookModel;
+using Nop.Core.Domain.Catalog;
+
+namespace Nop.Services.AIBookModel
+{
+    public partial class AiBookService : IAiBookService
+    {
+        public int DeleteAiBookModel(AiBookModel store)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AiBookModel GetAiBookModelById(int storeId, bool loadCacheableCopy = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<AiBookModel> GetAllAiBookModels(bool loadCacheableCopy = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] GetNotExistingAiBookModels(string[] storeIdsNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int InsertAiBookModel(AiBookModel bookdir)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPagedList<AiBookModel> SearchProducts(int pageIndex = 0, int pageSize = int.MaxValue, IList<int> categoryIds = null, int manufacturerId = 0, int storeId = 0, int vendorId = 0, bool visibleIndividuallyOnly = false, bool markedAsNewOnly = false, bool? featuredProducts = null, string keywords = null, bool searchDescriptions = false, bool searchManufacturerPartNumber = true, bool searchSku = true, bool searchProductTags = false, int languageId = 0, IList<int> filteredSpecs = null, ProductSortingEnum orderBy = ProductSortingEnum.Position, bool showHidden = false, bool? overridePublished = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UpdateAiBookModel(AiBookModel bookdir)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Libraries/Nop.Services/AIBookModel/IAiBookService.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Nop.Core;
+using Nop.Core.Domain.AIBookModel;
+using Nop.Core.Domain.Catalog;
+
+namespace Nop.Services.AIBookModel
+{
+   public interface IAiBookService
+    {
+        /// <summary>
+        /// Inserts a BookDir
+        /// </summary>
+        /// <param name="bookdir">bookdir</param>
+        int InsertAiBookModel(AiBookModel bookdir);
+
+        /// <summary>
+        /// Updates the BookDir
+        /// </summary>
+        /// <param name="bookdir">BookDir</param>
+        int UpdateAiBookModel(AiBookModel bookdir);
+
+
+        /// <summary>
+        /// Deletes a BookDir
+        /// </summary>
+        /// <param name="bookdir">bookdir</param>
+        int DeleteAiBookModel(AiBookModel store);
+
+
+
+        /// <summary>
+        /// Gets all stores
+        /// </summary>
+        /// <param name="loadCacheableCopy">A value indicating whether to load a copy that could be cached (workaround until Entity Framework supports 2-level caching)</param>
+        /// <returns>Stores</returns>
+        IList<AiBookModel> GetAllAiBookModels(bool loadCacheableCopy = true);
+
+
+        /// <summary>
+        /// Gets a store 
+        /// </summary>
+        /// <param name="storeId">Store identifier</param>
+        /// <param name="loadCacheableCopy">A value indicating whether to load a copy that could be cached (workaround until Entity Framework supports 2-level caching)</param>
+        /// <returns>Store</returns>
+        AiBookModel GetAiBookModelById(int storeId, bool loadCacheableCopy = true);
+
+
+        /// <summary>
+        /// Returns a list of names of not existing stores
+        /// </summary>
+        /// <param name="storeIdsNames">The names and/or IDs of the store to check</param>
+        /// <returns>List of names and/or IDs not existing stores</returns>
+        string[] GetNotExistingAiBookModels(string[] storeIdsNames);
+
+
+        /// <summary>
+        /// Search products
+        /// </summary>
+        /// <param name="pageIndex">Page index</param>
+        /// <param name="pageSize">Page size</param>
+        /// <param name="categoryIds">Category identifiers</param>
+        /// <param name="manufacturerId">Manufacturer identifier; 0 to load all records</param>
+        /// <param name="storeId">Store identifier; 0 to load all records</param>
+        /// <param name="vendorId">Vendor identifier; 0 to load all records</param>
+        /// <param name="warehouseId">Warehouse identifier; 0 to load all records</param>
+        /// <param name="productType">Product type; 0 to load all records</param>
+        /// <param name="visibleIndividuallyOnly">A values indicating whether to load only products marked as "visible individually"; "false" to load all records; "true" to load "visible individually" only</param>
+        /// <param name="markedAsNewOnly">A values indicating whether to load only products marked as "new"; "false" to load all records; "true" to load "marked as new" only</param>
+        /// <param name="featuredProducts">A value indicating whether loaded products are marked as featured (relates only to categories and manufacturers). 0 to load featured products only, 1 to load not featured products only, null to load all products</param>
+        /// <param name="priceMin">Minimum price; null to load all records</param>
+        /// <param name="priceMax">Maximum price; null to load all records</param>
+        /// <param name="productTagId">Product tag identifier; 0 to load all records</param>
+        /// <param name="keywords">Keywords</param>
+        /// <param name="searchDescriptions">A value indicating whether to search by a specified "keyword" in product descriptions</param>
+        /// <param name="searchManufacturerPartNumber">A value indicating whether to search by a specified "keyword" in manufacturer part number</param>
+        /// <param name="searchSku">A value indicating whether to search by a specified "keyword" in product SKU</param>
+        /// <param name="searchProductTags">A value indicating whether to search by a specified "keyword" in product tags</param>
+        /// <param name="languageId">Language identifier (search for text searching)</param>
+        /// <param name="filteredSpecs">Filtered product specification identifiers</param>
+        /// <param name="orderBy">Order by</param>
+        /// <param name="showHidden">A value indicating whether to show hidden records</param>
+        /// <param name="overridePublished">
+        /// null - process "Published" property according to "showHidden" parameter
+        /// true - load only "Published" products
+        /// false - load only "Unpublished" products
+        /// </param>
+        /// <returns>Products</returns>
+        IPagedList<AiBookModel> SearchProducts(
+            int pageIndex = 0,
+            int pageSize = int.MaxValue,
+            IList<int> categoryIds = null,
+            int manufacturerId = 0,
+            int storeId = 0,
+            int vendorId = 0,
+            bool visibleIndividuallyOnly = false,
+            bool markedAsNewOnly = false,
+            bool? featuredProducts = null,
+            string keywords = null,
+            bool searchDescriptions = false,
+            bool searchManufacturerPartNumber = true,
+            bool searchSku = true,
+            bool searchProductTags = false,
+            int languageId = 0,
+            IList<int> filteredSpecs = null,
+            ProductSortingEnum orderBy = ProductSortingEnum.Position,
+            bool showHidden = false,
+            bool? overridePublished = null);
+    }
+}
